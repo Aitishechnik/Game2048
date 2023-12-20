ecc219648dd58df43d20684b27df646bca4d01da
Aitishechnik <aitishechka666@gmail.com>
2023-12-21 00:47:32 +0300

    Update (working animation)

--- Unity2048/Assets/Scripts/FlyTilesController.cs
+++ Unity2048/Assets/Scripts/FlyTilesController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class FlyTilesController : MonoBehaviour
 {
     [SerializeField]
     private TileFactory _tileFactory;
+
     public void Fly(TileView from, TileView to, float time)
     {
         StartCoroutine(FlyRoutine(from, to, time));
@@ -13,14 +18,28 @@ public class FlyTilesController : MonoBehaviour
 
     private IEnumerator FlyRoutine(TileView from, TileView to, float time)
     {
-        float startTime = Time.time;
-        var tile = _tileFactory.Create(from.Tile, transform);
-        tile.transform.position = from.transform.position;
-        while(Time.time < startTime + time)
+        float elapsedTime = 0f;
+
+        var tile = TileFactory.Instance.Create(from.Tile, transform);
+        tile.transform.GetChild(0).gameObject.SetActive(false);
+        var sorting = tile.AddComponent<SortingGroup>();
+        sorting.sortAtRoot = true;
+
+        while (elapsedTime < time)
         {
-            Vector3.Lerp(from.transform.position, to.transform.position, (startTime + time - Time.time)/time);
+            float t = elapsedTime / time;
+            tile.transform.position = Vector3.Lerp(from.transform.position, to.transform.position, t);
+
+            elapsedTime += Time.deltaTime;
             yield return null;
         }
+
+        sorting.sortAtRoot = false;
+        Destroy(sorting);
+
         tile.transform.position = to.transform.position;
+
+        tile.ReturnToPool();
+        from.Tile.SetValue(from.ThisTileData.Value);
     }
 }
